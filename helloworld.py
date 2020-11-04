4918ebfee3332ffd84c933e18ad9b943b0e21cf2
DK Kim <dk2kim@gmail.com>
2020-11-04 10:43:10 -0500

    update to hello message and comments

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 def hello():
 	print('Hello World')
 
-# this statement helps with running hello() whenever this
-# file is ran from the command line
 if __name__ == '__main__':
 	hellow()
+
+# changes made
